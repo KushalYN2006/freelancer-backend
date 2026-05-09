c4cd7af2e64d17c136c226530c0fdd18c9978031
KushalYN2006 <kushalyn2006@gmail.com>
2026-05-09 16:30:35 +0530

    Initial backend - FreelancerHub Spring Boot app

---
+++ src/main/java/com/freelancerhub/FreelancerHubApplication.java
@@ -0,0 +1,11 @@
+package com.freelancerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FreelancerHubApplication.class, args);
+    }
+}
